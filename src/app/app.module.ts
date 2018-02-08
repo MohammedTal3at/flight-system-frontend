b7533d27bc21af2845fd2f77fa141c63f8ed0b36
sandraraouf <sandraraouf@gmail.com>
2018-02-08 16:38:28 +0200

    add signin and signup components and shared folder that having AuthService and modify app.module-app.component-index.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
+//Authservice-->put it in the providers
+import { AuthServiceService } from './shared/Auth/auth-service.service';
+//Http
+//put it in the imports
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+//Routing
+import { RouterModule, Routes } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    //Routing
+    RouterModule.forRoot([
+      {
+      path:"signin",component:SigninComponent,
+      }
+  ,{
+    path:"signup",component:SignupComponent,
+  }
+  ]),
   ],
-  providers: [],
+  providers: [AuthServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
