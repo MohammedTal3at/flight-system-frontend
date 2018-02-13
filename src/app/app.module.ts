9003b4eab8993d5f84c69485f01dab4cd2ed70a1
evamagdy <eva.magdy.17@gmail.com>
2018-02-13 15:04:53 +0200

    design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,25 +15,68 @@ import { FormsModule } from '@angular/forms';
 
 //Routing
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+
+import { ProfileComponent } from './profile/profile.component';
+////bootstrap
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { TripsComponent } from './trips/trips.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    AboutUsComponent,
+    ContactUsComponent,
+  
+    ProfileComponent,
+  
+    TripsComponent,
+  
+
+    
   ],
   imports: [
     BrowserModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     FormsModule,
     HttpModule,
     //Routing
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot(),
     RouterModule.forRoot([
       {
+       path:"profile",component :  ProfileComponent,
+      },
+      {
+        path:"contact-us",component:ContactUsComponent,
+      },
+      {
+        path:"trips",component:TripsComponent,
+      },
+      {
       path:"signin",component:SigninComponent,
+      },
+      {
+      path:"about-us", component: AboutUsComponent
       }
-  ,{
+     ,{
     path:"signup",component:SignupComponent,
-  }
+  },
+  {
+    path:'**', component: HomeComponent
+  },  
   ]),
   ],
   providers: [AuthServiceService],
