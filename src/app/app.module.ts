1a32cecf4bc9b5c123754c9df5eeaf87fe487521
ashrafemad <ashrafemad23@gmail.com>
2018-03-06 13:23:25 +0200

    initializing front-end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,21 @@ import { AppComponent } from './app.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
-//Authservice-->put it in the providers
+// Authservice-->put it in the providers
 import { AuthServiceService } from './shared/Auth/auth-service.service';
-//Http
-//put it in the imports
+// Http
+// put it in the imports
 import { HttpModule } from '@angular/http';
 import { FormsModule } from '@angular/forms';
 
-//Routing
+// Routing
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 
 import { ProfileComponent } from './profile/profile.component';
-////bootstrap
+//// bootstrap
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ModalModule } from 'ngx-bootstrap/modal';
